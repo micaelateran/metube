8514059c358d9e7abf9c1505af9f0cfc693b66c9
AngelUrq <angel4441916@gmail.com>
2018-05-03 21:52:36 -0400

    Problema de la extensión solucionado

--- src/app/interfaz-retos/interfaz-retos.component.ts
+++ src/app/interfaz-retos/interfaz-retos.component.ts
@@ -15,17 +15,17 @@ export class InterfazRetosComponent implements OnInit {
   }
 
   reto1(): void{
-    this.data.setVideoID("https://firebasestorage.googleapis.com/v0/b/metube-120e9.appspot.com/o/test%2F1525036482246_Trucos%20de%20yoyo.mp4?alt=media&token=f8877c1a-3dbd-4795-96b0-82c01b65bb1d");
+    this.data.setVideoID("/v0/b/metube-120e9.appspot.com/o/test%2F1525036482246_Trucos%20de%20yoyo.mp4?alt=media&token=f8877c1a-3dbd-4795-96b0-82c01b65bb1d");
     this.router.navigateByUrl('/watch');
   }
 
   reto2(): void{
-    this.data.setVideoID("https://firebasestorage.googleapis.com/v0/b/metube-120e9.appspot.com/o/test%2F1525036482246_Trucos%20de%20yoyo.mp4?alt=media&token=f8877c1a-3dbd-4795-96b0-82c01b65bb1d");
+    this.data.setVideoID("/v0/b/metube-120e9.appspot.com/o/test%2F1525036482246_Trucos%20de%20yoyo.mp4?alt=media&token=f8877c1a-3dbd-4795-96b0-82c01b65bb1d");
     this.router.navigateByUrl('/watch');
   }
 
   reto3(): void{
-    this.data.setVideoID("https://firebasestorage.googleapis.com/v0/b/metube-120e9.appspot.com/o/test%2F1525039735479_Reto%20perrito.mp4?alt=media&token=8ac8a107-0c8c-467f-97e6-664b2522fb6d");
+    this.data.setVideoID("/v0/b/metube-120e9.appspot.com/o/test%2F1525039735479_Reto%20perrito.mp4?alt=media&token=8ac8a107-0c8c-467f-97e6-664b2522fb6d");
     this.router.navigateByUrl('/watch');
   }
 
