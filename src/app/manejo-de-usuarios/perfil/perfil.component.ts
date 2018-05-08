085cc3d578693de443b7ad445b995ed24931dafe
AngelUrq <angel4441916@gmail.com>
2018-05-08 11:19:34 -0400

    Perfil casi funcional

--- src/app/manejo-de-usuarios/perfil/perfil.component.ts
+++ src/app/manejo-de-usuarios/perfil/perfil.component.ts
@@ -1,6 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../servicios/data.service';
 import { AuthService } from '../../servicios/auth.service';
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+
+interface Video{
+  calificacion: number;
+  codigoReto: string;
+  codigoUsuario: string;
+  codigoVideo: string;
+  miniaturaUrl: string;
+  videoUrl: string;
+}
+
+interface Reto{
+  codigoReto: string;
+  descripcion: string;
+  nombre: string;
+  urlMiniatura: string;
+}
 
 @Component({
   selector: 'app-perfil',
@@ -8,6 +27,12 @@ import { AuthService } from '../../servicios/auth.service';
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent implements OnInit {
+
+  coleccionDeVideos: AngularFirestoreCollection<Video>;
+  coleccionDeRetos: AngularFirestoreCollection<Reto>;
+
+  videos: Observable<Video[]>;
+  retos: Observable<Reto[]>;
 
   login: boolean;
 
@@ -18,7 +43,7 @@ export class PerfilComponent implements OnInit {
 
   cantidadVideos: number;
 
-  constructor(private data: DataService, public authService: AuthService) { }
+  constructor(private router: Router, private afs: AngularFirestore, private data: DataService, public authService: AuthService) { }
 
   ngOnInit() {
     this.login = this.data.getLogin();
@@ -42,6 +67,16 @@ export class PerfilComponent implements OnInit {
         this.data.setLogin(false);
       }
     });
+
+    this.coleccionDeVideos = this.afs.collection('Videos', ref => {return ref.where('codigoUsuario','==', this.data.getUserID())});
+    this.coleccionDeRetos = this.afs.collection('Retos', ref => {return ref.where('codigoReto','==', this.data.getRetoID())})
+
+    this.videos = this.coleccionDeVideos.valueChanges();
+    this.retos = this.coleccionDeRetos.valueChanges();
+  }
+
+  verVideo(codigoVideo: string){
+    this.router.navigateByUrl("/watch?" + codigoVideo + "=")
   }
 
   conteoVideos(){
