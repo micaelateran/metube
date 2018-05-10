2790957a8f28cdeeb86d19133f7af76a24cae44a
jxuxaxcxo <joaquin.vargaselio@gmail.com>
2018-05-10 12:05:56 -0400

    HEROKU 14

--- server.js
+++ server.js
@@ -3,27 +3,22 @@ const app = express();
 const path = require ('path');
 
 
-app.use(express.static(__dirname + '/dist'));
-
-app.use(function(req, res, next) {
-    var allowedOrigins = ['https://firebasestorage.googleapis.com'];
-    var origin = req.headers.origin;
-    if(allowedOrigins.indexOf(origin) > -1){
-         res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://firebasestorage.googleapis.com');
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
-    return next();
-})
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-app.listen(process.env.PORT || 8080);
+    next();
+}
 
-//PathLocationStrategy
-app.get('/*', function(req,res){
-
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
-
-console.log ('Console listening');
+//...
+app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: 'cool beans' }));
+    app.use(express.methodOverride());
+    app.use(allowCrossDomain);
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
