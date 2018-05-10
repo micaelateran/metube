858987cabde2fcd283470d9e65e98c516ba9f770
jxuxaxcxo <joaquin.vargaselio@gmail.com>
2018-05-10 12:15:25 -0400

    HEROKU 15

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const path = require ('path');
 
 
 app.use(express.static(__dirname + '/dist'));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 app.listen(process.env.PORT || 8080);
 
