ac330e76476b17a355299b062cdb89a3519321f4
jxuxaxcxo <joaquin.vargaselio@gmail.com>
2018-05-10 12:10:38 -0400

    HEROKU 15

--- server.js
+++ server.js
@@ -3,22 +3,14 @@ const app = express();
 const path = require ('path');
 
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://firebasestorage.googleapis.com');
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+app.use(express.static(__dirname + '/dist'));
 
-    next();
-}
+app.listen(process.env.PORT || 8080);
 
-//...
-app.configure(function() {
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.session({ secret: 'cool beans' }));
-    app.use(express.methodOverride());
-    app.use(allowCrossDomain);
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
-});
+//PathLocationStrategy
+app.get('/*', function(req,res){
+
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
+console.log ('Console listening');
